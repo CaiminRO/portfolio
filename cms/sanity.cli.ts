d4d0cd22935cd2be5a1f9e3cbdd7fbb549faa5ae
CaiminRO <caiminr01@gmail.com>
2024-12-10 13:56:19 -0800

    refactor: add env support to generalize cms

--- cms/sanity.cli.ts
+++ cms/sanity.cli.ts
@@ -1,13 +1,10 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'ettt9pum',
-    dataset: 'production'
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_DATASET
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
+  studioHost: process.env.SANITY_STUDIO_API_HOST,
   autoUpdates: true,
 })
--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -1,14 +1,14 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'Portfolio CMS',
 
-  projectId: 'ettt9pum',
-  dataset: 'production',
+  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID || '',
+  dataset: process.env.SANITY_STUDIO_API_DATASET || '',
 
   plugins: [structureTool(), visionTool()],
 
