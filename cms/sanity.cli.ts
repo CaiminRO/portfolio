88b98cf07a2e8ceb4e9bbdd2568bfd109fd06ee7
CaiminRO <89493663+CaiminRO@users.noreply.github.com>
2024-12-10 14:10:19 -0800

    Sanity CMS Initial Setup (#3)
    * feat: init Sanity project
    
    * refactor: add env support to generalize cms
    
    * docs: notes for Sanity in root README

---
+++ cms/sanity.cli.ts
@@ -0,0 +1,10 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_DATASET
+  },
+  studioHost: process.env.SANITY_STUDIO_API_HOST,
+  autoUpdates: true,
+})
