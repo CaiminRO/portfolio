e496b128fe4d7e5dbc694b48e3bec8e4592cd7db
CaiminRO <caiminr01@gmail.com>
2024-12-15 20:47:15 -0800

    feat: Contact Button - bottom right of Home

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+import ContactButton from "@/components/common/contact/ContactButton";
 import Hero from "@/components/common/Hero";
+import { useState } from "react";
 
 export default function Home() {
   return <Hero />;
+  const [isContactOpen, setIsContactOpen] = useState(false);
+
+  return (
+    <>
+      <Hero/>
+      <ContactButton onClick={() => setIsContactOpen(true)}/>
+    </>
+  );
 }
