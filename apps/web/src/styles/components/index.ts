ace994883c8d4f6851151d208698f9add0726581
CaiminRO <caiminr01@gmail.com>
2025-01-12 09:31:50 -0800

    feat: component for Icons for common links

--- apps/web/src/styles/components/index.ts
+++ apps/web/src/styles/components/index.ts
@@ -2,5 +2,13 @@ import headerStyles from "./Header.module.css";
 import footerStyles from "./Footer.module.css";
 import heroStyles from "./Hero.module.css";
 import contactStyles from "./Contact.module.css";
+import linkIconStyles from "./LinkIcon.module.css";
 
 export { headerStyles, footerStyles, heroStyles, contactStyles };
+export {
+  headerStyles,
+  footerStyles,
+  heroStyles,
+  contactStyles,
+  linkIconStyles,
+};
