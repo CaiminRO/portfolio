568f05278d4baceb87ad2007915e3d9f7cd9d832
CaiminRO <caiminr01@gmail.com>
2025-01-13 05:00:45 -0800

    feat: util functions for data formatting and portfolio querying

--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -2,3 +2,5 @@ export * from "@/config/Socials/util";
 export * from "@/config/Personal/util";
 export * from "@/config/Resend/util";
 export * from "./sanity";
+export * from "./formatDate";
+export * from "./portfolio";
